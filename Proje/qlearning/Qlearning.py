5e3e66e2a26a2736f78c51e01f16e81d400c2b7b
Kadir Deliceli <d.kadir5734@gmail.com>
2025-12-24 21:39:23 +0300

    Delete Proje directory

--- Proje/qlearning/Qlearning.py
+++
@@ -1,6 +0,0 @@
-from genetik.ga import run_genetic_algorithm
-
-
-def Q_Learning_run(G, s, d, talep, weights):
-    print("Q-learning Çalıştı")
-    return run_genetic_algorithm(G, s, d, talep, weights)
